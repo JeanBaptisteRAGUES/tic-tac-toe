5c3ac84b81c8d147c762ea47b6d5c47d5d66627a
JeanBaptisteRAGUES <jeanbonduel313@gmail.com>
2021-08-17 02:44:18 +0200

    Programmation des matchs avec affichage en temps réel des cases cochées par le joueur ou par l'adversaire ainsi qu'un message qui dit à qui est le tour. Vérification que la case sélectionnée est bien valide et vérification que ce soit à notre tour de jouer ou pas.

--- src/components/Match/index.js
+++ src/components/Match/index.js
@@ -1,13 +1,121 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { FirebaseContext } from '../Firebase';
+import './match.css';
 
 const Match = () => {
 
     const {matchid} = useParams();
+    const firebase = useContext(FirebaseContext);
+    const [match, setMatch] = useState(null);
+    const [grid, setGrid] = useState([]);
+    const [team, setTeam] = useState('');
+
+    useEffect(() => {
+
+        firebase.auth.onAuthStateChanged((user) => {
+            if(user){
+                let uId = user.uid;
+                const unsubscribe = uId != null && firebase.db.collection('matches').doc(matchid)
+                .onSnapshot(match => {
+                    const matchData = match.data();
+                    setMatch(matchData);
+                    setGrid(JSON.parse(matchData.grid));
+                    setTeam(getTeam(matchData, uId));
+                    console.log(grid);
+                });
+        
+                return () => unsubscribe();
+            }
+        });
+    }, []);
+
+    const getUserId = () => {
+        return firebase.auth.currentUser.uid;
+    }
+
+    const getTeam = (mData, playerId) => {
+        return mData.playerX_id == playerId ? 'X' : 'O'
+    }
+
+    const isPlayerTurn = (playerId) => {
+        return playerId === match.currentPlayer;
+    }
+
+    const changePlayer = () => {
+        if(match.currentPlayer === match.playerX_id){
+            return match.playerO_id;
+        }
+        return match.playerX_id;
+    }
+
+    const isCaseValid = (rI, cI) => {
+        return grid[rI][cI] === '';
+    }
+
+    const handleClick = (rI, cI) => {
+        if(isPlayerTurn(getUserId())){
+            if(isCaseValid(rI,cI)){
+                const newGrid = grid.slice(0);
+                newGrid[rI][cI] = team;
+                //setGrid(newGrid); 
+                firebase.db.collection('matches').doc(matchid)
+                .update(
+                    {
+                        grid: JSON.stringify(newGrid),
+                        turn: match.turn+1,
+                        currentPlayer: changePlayer()
+                    }
+                );
+            }else{
+                console.log("Cette case est déjà prise !");
+            }
+        }else{
+            console.log("Ce n'est pas à votre tour !");
+        }
+    }
+
+    const displayMatch = match != null && (
+        <div className='matchDisplay'>
+            <h2>{match.playerX_username} VS {match.playerO_username} : </h2>
+            <div className='gridBox'>
+            {
+                grid.map((row, rowIndex) => (
+                    row.map((gCase, colIndex) => (
+                        <div key={(rowIndex+1) * (colIndex+1)} className='gridCase' onClick={() => handleClick(rowIndex, colIndex)}>
+                            {gCase}
+                        </div>
+                    ))
+                ))
+            }
+            </div>
+        </div>
+    )
+
+    /*
+    if(getUserId() === match.currentPlayer){
+                    
+                }else{
+                    
+                }
+                */
+
+    const playerTurn = team != '' && (
+        <div className='turnMessage'>
+            {
+                match.currentPlayer === getUserId() ? 
+                <p>A vous de jouer !</p>
+                :
+                <p>A l'adversaire de jouer.</p>
+            }
+        </div>
+    )
 
     return (
         <div>
             <h1>ID match : {matchid}</h1>
+            {displayMatch}
+            {playerTurn}
         </div>
     )
 }
